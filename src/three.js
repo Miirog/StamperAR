7d4dcfa6e50f399ad0f12be807a765a93cc606ea
Igor Dal Col <igormdalcol@gmail.com>
2025-02-18 14:15:33 -0300

    fix import

--- src/three.js
+++ src/three.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const nextButton = document.getElementById("nextObject");
 const previousButton = document.getElementById("previousObject");
