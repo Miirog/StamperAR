d13ec8f698f736a581ec732c2d83d3975ced96c2
Igor Dal Col <igormdalcol@gmail.com>
2025-02-18 12:50:42 -0300

    deploy test

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/StamperAR/', // Replace with your repo name!
+  build: {
+    outDir: 'dist', // Or whatever your output directory is
+  },
+});
